fd38cae3053ca123994c6b1d663af8f6b5256330
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-10-04 21:30:30 -0300

    refactor: socket namespace for movieNote

--- src/useCase/movie/movieNote/MovieNoteUseCase.ts
+++ src/useCase/movie/movieNote/MovieNoteUseCase.ts
@@ -2,15 +2,11 @@ import { io } from '../../../server';
 
 export class MovieNoteUseCase {
   emitMovieNoteEvents(movieId: string, event: string, content: JSON) {
-    const room = `/movie/${movieId}/note`;
-    io.of(room).emit(event, {
-      event: event,
-      content,
-    });
+    io.of('/movie/note').emit(event, content);
 
     return {
       sucess: true,
-      message: `Event ${event} emmitted on room ${room}`,
+      message: `Event ${event} emmitted on room ${movieId}`,
     };
   }
 }
